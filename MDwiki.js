552cc70b47a8d095015e6509485ff55b9bf830bc
lietxia <lietxia@gmail.com>
2018-08-21 17:24:45 +0800

    fix

--- MDwiki.js
+++ MDwiki.js
@@ -1438,7 +1438,7 @@ if (typeof exports === 'object') {
             var href = $el.attr('href');
             var text = $el.toptext();
             $.ajax({
-                url: href+"?"+Math.random(),
+                url: href+"?v="+Math.random(),
                 dataType: 'text'
             })
             .done(function (data) {
