a305f98f1ef751ebe690a5a0ff8ac0e4c87ac276
lietxia <lietxia@gmail.com>
2018-11-14 12:17:53 +0800

    update

--- res/full.js
+++ res/full.js
@@ -5,11 +5,10 @@ var new_script = document.createElement("script");
 new_script.setAttribute("src", "http://push.zhanzhang.baidu.com/push.js?123");
 document.body.appendChild(new_script);
 
-var new_script = document.createElement("script");
-
 
 //载入全屏脚本
 var new_script = document.createElement("script");
 new_script.setAttribute("src", "https://lietxia.github.io/res/screenfull.min.js");
-new_script.async = true;
 document.body.appendChild(new_script);
+
+screenfull.request();
