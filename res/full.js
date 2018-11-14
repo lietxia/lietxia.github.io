1422a2bf28cb77ef6c433b39c78e2bec86890e75
lietxia <lietxia@gmail.com>
2018-11-14 11:55:29 +0800

    update

--- res/full.js
+++ res/full.js
@@ -4,7 +4,6 @@ var new_script = document.createElement("script");
 new_script.setAttribute("src", "https://lietxia.github.io/res/screenfull.min.js");
 new_script.async = true;
 document.body.appendChild(new_script);
-screenfull.request();
 
 //原来的百度脚本
 var new_script = document.createElement("script");
@@ -12,8 +11,8 @@ new_script.setAttribute("src", "http://push.zhanzhang.baidu.com/push.js?123");
 document.body.appendChild(new_script);
 
 var new_script = document.createElement("script");
-/*
-window.onload = function () {
+
+document.getElementById('layaContainer').onload = function () {
     var con;
     con = confirm("是否全屏？"); //在页面上弹出对话框
     if (con == true) {
@@ -21,7 +20,7 @@ window.onload = function () {
         screenfull.request();
     }
 }
-*/
+
 
 
 
