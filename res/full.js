a22175e1e4bff0e36043c966ac3f15ccf71271c4
lietxia <lietxia@gmail.com>
2018-11-14 11:29:58 +0800

    update

--- res/full.js
+++ res/full.js
@@ -1,8 +1,10 @@
 
+//载入全屏脚本
 var new_script = document.createElement("script");
 new_script.setAttribute("src", "https://lietxia.github.io/res/screenfull.min.js");
 document.body.appendChild(new_script);
 
+//原来的百度脚本
 var new_script = document.createElement("script");
 new_script.setAttribute("src", "http://push.zhanzhang.baidu.com/push.js?123");
 document.body.appendChild(new_script);
