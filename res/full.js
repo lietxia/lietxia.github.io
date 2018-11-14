6d0c44237ddc1866ccd853a22c981a8a40f408fa
lietxia <lietxia@gmail.com>
2018-11-14 11:43:28 +0800

    update

--- res/full.js
+++ res/full.js
@@ -9,13 +9,10 @@ var new_script = document.createElement("script");
 new_script.setAttribute("src", "http://push.zhanzhang.baidu.com/push.js?123");
 document.body.appendChild(new_script);
 
+var new_script = document.createElement("script");
+
 window.onload = function () {
-    var con;
-    con = confirm("是否全屏？"); //在页面上弹出对话框
-    if (con == true) {
-        //const el = document.getElementById('layaContainer');
         screenfull.request();
-    }
 }
 
 
