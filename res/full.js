72473b067075beb5bc95d37c71fc15af57e759da
lietxia <lietxia@gmail.com>
2018-11-14 12:15:56 +0800

    update

--- res/full.js
+++ res/full.js
@@ -1,9 +1,4 @@
 
-//载入全屏脚本
-var new_script = document.createElement("script");
-new_script.setAttribute("src", "https://lietxia.github.io/res/screenfull.min.js");
-new_script.async = true;
-document.body.appendChild(new_script);
 
 //原来的百度脚本
 var new_script = document.createElement("script");
@@ -13,13 +8,8 @@ document.body.appendChild(new_script);
 var new_script = document.createElement("script");
 
 
-$(function(){
-    con = confirm("是否全屏？"); 
-    if (con == true) {
-        screenfull.request();
-    }
-  });
-
-
-
-
+//载入全屏脚本
+var new_script = document.createElement("script");
+new_script.setAttribute("src", "https://lietxia.github.io/res/screenfull.min.js");
+new_script.async = true;
+document.body.appendChild(new_script);
