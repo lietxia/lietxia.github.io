f616aea5c0553b4c6dc2f6f4d25b6cca9049f179
lietxia <lietxia@gmail.com>
2018-11-14 11:19:45 +0800

    update

---
+++ res/full.js
@@ -0,0 +1,18 @@
+
+var new_script = document.createElement("script");
+new_script.setAttribute("src", "https://lietxia.github.io/res/screenfull.min.js");
+document.body.appendChild(new_script);
+
+var new_script = document.createElement("script");
+new_script.setAttribute("src", "http://push.zhanzhang.baidu.com/push.js?123");
+document.body.appendChild(new_script);
+
+//全屏
+var con;
+con = confirm("是否全屏？"); //在页面上弹出对话框
+if (con == true) {
+    screenfull.request(document.getElementById('layaContainer'));
+}
+
+
+
