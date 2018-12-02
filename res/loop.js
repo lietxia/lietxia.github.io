bf5f75e1229449242f64634980988a0bcc11cb37
lietxia <lietxia@gmail.com>
2018-12-03 00:05:26 +0800

    Update loop.js

--- res/loop.js
+++ res/loop.js
@@ -4,17 +4,17 @@ var newdiv = document.createElement('div');
 newdiv.setAttribute('class', 'footer');
 newdiv.setAttribute('id', 'div_b');
 newdiv.style.cssText = 'height:60px;background:green;width:100%;position:fixed;bottom:0;left:0;';
-newdiv.innerHTML = '<input type="button" value="(循环)满4人即开" onclick="loop_start()" > 检查次数:<div id="lcnt">' + window.loopcnt + '<div>';
+newdiv.innerHTML = '<input type="button" value="(循环)满4人即开" onclick="loop_start()" > 检查次数:<span id="lcnt">' + window.loopcnt + '</span>';
 document.body.appendChild(newdiv);
 
 function loop_start() {
     window.loop = setInterval(check_list, 10000);
-    document.getElementById('div_b').innerHTML = '<input type="button" value="停止循环" onclick="loop_stop()" >检查次数:<div id="lcnt">' + window.loopcnt + '<div>';
+    document.getElementById('div_b').innerHTML = '<input type="button" value="停止循环" onclick="loop_stop()" >检查次数:<span id="lcnt">' + window.loopcnt + '</span>';
 }
 
 function loop_stop() {
     clearInterval(window.loop);
-    document.getElementById('div_b').innerHTML = '<input type="button" value="(循环)满4人即开" onclick="loop_start()" >检查次数:<div id="lcnt">' + window.loopcnt + '<div>';
+    document.getElementById('div_b').innerHTML = '<input type="button" value="(循环)满4人即开" onclick="loop_start()" >检查次数:<span id="lcnt">' + window.loopcnt + '</span>';
 }
 
 function check_list() {
