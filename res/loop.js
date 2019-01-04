186a3263aedaa9ff569ca47106d6befad38d2e24
lietxia <lietxia@gmail.com>
2019-01-05 01:51:29 +0800

    fix bug

--- res/loop.js
+++ res/loop.js
@@ -82,25 +82,25 @@ async function tenhou_log() {
   alert("读取成功，可以利用【天凤统计工具】进行更详细的统计");
 }
 
-async function loop_start() {
-  var tag_span = document.getElementsByTagName("span");
-  for (var i = 0; i < tag_span.length; i++) {
-    if (tag_span[i].innerText === "对局管理") {
-      tag_span[i].setAttribute("id", "sp_manage");
+function loop_start() {
+  var _span_ = document.getElementsByTagName("span");
+  for (var i = 0; i < _span_.length; i++) {
+    if (_span_[i].innerText === "对局管理") {
+      _span_[i].setAttribute("id", "sp_st");
     }
-    if (tag_span[i].innerText === "赛事设定") {
-      tag_span[i].setAttribute("id", "sp_set");
+    if (_span_[i].innerText === "赛事设定") {
+      _span_[i].setAttribute("id", "sp_set");
     }
   }
   if (
-    document.getElementById("sp_manage") == null ||
-    document.getElementById("sp_manage") == ""
+    document.getElementById("sp_set") == null ||
+    document.getElementById("sp_set") == ""
   ) {
     return alert("请进入开比赛的页面执行此脚本");
   }
+
   document.getElementById("sp_set").click();
-  await sleep(2000);
-  document.getElementById("sp_manage").click();
+  setTimeout("document.getElementById('sp_st').click()", 1000);
   window.loopcnt = 0;
 
   window.loop = setInterval(check_list, 10000);
@@ -118,43 +118,35 @@ function loop_stop() {
   document.getElementById("lcnt").innerText = window.loopcnt;
 }
 
-async function stck() {
+function stck() {
   window.loopcnt++;
   var pcnt = 0;
-  var tag_span = document.getElementsByTagName("span");
-  for (var i = 0; i < tag_span.length; i++) {
-    if (tag_span[i].innerText === "准备开始") {
+  var _span_ = document.getElementsByTagName("span");
+  for (var i = 0; i < _span_.length; i++) {
+    if (_span_[i].innerText === "准备开始") {
       pcnt++;
-      tag_span[i].parentNode.setAttribute("id", "sbt_" + pcnt);
+      _span_[i].parentNode.setAttribute("id", "sbt_" + pcnt);
     }
-    if (tag_span[i].innerText === "对局开始") {
-      tag_span[i].parentNode.setAttribute("id", "btn_st");
+    if (_span_[i].innerText === "对局开始") {
+      _span_[i].parentNode.setAttribute("id", "btn_st");
     }
-    if (tag_span[i].innerText === "随机坐席（后台）") {
-      tag_span[i].parentNode.setAttribute("id", "btn_rand");
+    if (_span_[i].innerText === "随机坐席（后台）") {
+      _span_[i].parentNode.setAttribute("id", "btn_rand");
     }
   }
   if (pcnt >= 4) {
     document.getElementById("sbt_1").click();
-    await sleep(1500);
-    document.getElementById("sbt_2").click();
-    await sleep(1500);
-    document.getElementById("sbt_3").click();
-    await sleep(1500);
-    document.getElementById("sbt_4").click();
-    await sleep(1500);
-    document.getElementById("btn_rand").click();
-    await sleep(3000);
-    document.getElementById("btn_st").click();
+    setTimeout("document.getElementById('sbt_2').click()", 1000);
+    setTimeout("document.getElementById('sbt_3').click()", 2000);
+    setTimeout("document.getElementById('sbt_4').click()", 3000);
+    setTimeout("document.getElementById('btn_rand').click()", 4000);
+    setTimeout("document.getElementById('btn_st').click()", 5000);
   }
   document.getElementById("lcnt").innerText = window.loopcnt;
 }
 
-async function check_list() {
-  //循环检查
+function check_list() {
   document.getElementById("sp_set").click();
-  await sleep(2000);
-  document.getElementById("sp_manage").click();
-  await sleep(2000);
-  stck();
+  setTimeout("document.getElementById('sp_st').click()", 1000);
+  setTimeout("stck()", 2000);
 }
