eba85415739b6cbab0b12e9b0917fc82837672e5
lietxia <lietxia@gmail.com>
2019-01-16 17:43:44 +0800

    1

--- MDwiki.js
+++ MDwiki.js
@@ -2657,12 +2657,12 @@ function googlemapsReady() {
     'use strict';
 
     var themes = [
-        { name: '初始', url: 'cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css' },
-        { name: '蓝色', url: 'cdn.bootcss.com/bootswatch/3.3.7/cerulean/bootstrap.min.css' },
-        { name: '深色', url: 'cdn.bootcss.com/bootswatch/3.3.7/darkly/bootstrap.min.css' },
-        { name: '黑色', url: 'cdn.bootcss.com/bootswatch/3.3.7/cyborg/bootstrap.min.css' },
-        { name: '灰色', url: 'cdn.bootcss.com/bootswatch/3.3.7/slate/bootstrap.min.css' },
-        { name: '橙色', url: 'cdn.bootcss.com/bootswatch/3.3.7/united/bootstrap.min.css' },
+        { name: '初始', url: 'cdn.bootcss.com/bootstrap/3.4.0/css/bootstrap.min.css' },
+        { name: '蓝色', url: 'cdn.bootcss.com/bootswatch/3.4.0/cerulean/bootstrap.min.css' },
+        { name: '深色', url: 'cdn.bootcss.com/bootswatch/3.4.0/darkly/bootstrap.min.css' },
+        { name: '黑色', url: 'cdn.bootcss.com/bootswatch/3.4.0/cyborg/bootstrap.min.css' },
+        { name: '灰色', url: 'cdn.bootcss.com/bootswatch/3.4.0/slate/bootstrap.min.css' },
+        { name: '橙色', url: 'cdn.bootcss.com/bootswatch/3.4.0/united/bootstrap.min.css' },
     ];
     var useChooser = false;
     var themeChooserGimmick = {
